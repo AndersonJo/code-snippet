3d0c40b47312975c291161f6a61e6d139b678abf
Anderson <a141890@gmail.com>
2020-08-26 05:25:46 +0900

    asd

---
+++ 200-Kubernetes/README.md
@@ -0,0 +1,15 @@
+# Tutorial 
+
+## Preparation
+
+ 1. 먼저 [Kaggle Fahsion MNIST](https://www.kaggle.com/zalando-research/fashionmnist) 에서 데이터를 다운로드 받습니다.
+ 
+
+
+## Docker 확인
+
+```bash
+docker build -t myapp .
+docker run -p 5000:5000 myapp
+```
+
--- 200-Kubernetes/app/app.py
+++ 200-Kubernetes/app/app.py
@@ -1,8 +1,31 @@
-from flask import Flask
+from tempfile import gettempdir
+
+import cv2
+import numpy as np
+from flask import Flask, request, jsonify
+from keras.models import load_model
 
 app = Flask(__name__)
+model = load_model('fasion_model.h5')
 
 
 @app.route('/')
 def hello_world():
     return 'Hello! Anderson!'
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    tmp_dir = gettempdir()
+    f = request.files["image"]
+    f.save(tmp_dir + '/img.jpg', cv2.IMREAD_COLOR)
+    img = cv2.imread(tmp_dir + '/img.jpg', cv2.IMREAD_COLOR)[:, :, 0]
+    img = np.expand_dims(img, axis=0)
+    pred_y = model.predict(img)[0]
+    pred_label = int(np.argmax(pred_y))
+    prob = float(pred_y[pred_label])
+    return jsonify({'prediction': pred_label, 'prob': prob})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
